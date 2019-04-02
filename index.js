4e3302b393677ae92bfdad32d64b04012d2460f6
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2019-04-02 00:26:34 +0000

    Version v0.1

---
+++ index.js
@@ -0,0 +1,23 @@
+let deferredPrompt;
+const btnAdd = document.querySelector('.btn-install');
+
+window.addEventListener('beforeinstallprompt', (e)=>{
+    e.preventDefault();
+    deferredPrompt = e;
+    // Bouton invitant l'utilisateur a ajouter a l'ecran d'accueil
+    btnAdd.style.display = 'block';
+});
+
+btnAdd.addEventListener('click', (e) =>{
+    btnAdd.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice
+        .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+});
