627d12ec194c974b6060dc6ca419b50afc464d03
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2019-07-19 13:39:00 +0000

    Modification du programme

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open('v0.8').then(function (cache) {
+        caches.open('v0.9').then(function (cache) {
             return cache.addAll([
                 'index.html',
                 'comite.html',
@@ -124,9 +124,9 @@ self.addEventListener('install', function (event) {
                 'images/go-to-back.png',
                 '11.html',
                 '12.html',
-                '14.html',
-                '15.html',
-                '16.html',
+                //'14.html',
+                //'15.html',
+                //'16.html',
                 'precamp.html',
                 'precamp_samedi.html',
                 'compteur/timeTo.css',
@@ -143,7 +143,7 @@ self.addEventListener('fetch', function (event) {
         caches.match(event.request).then(function (resp) {
             return resp || fetch(event.request).then(function (response) {
                 let responseClone = response.clone();
-                caches.open('v0.8').then(function (cache) {
+                caches.open('v0.9').then(function (cache) {
                     cache.put(event.request, responseClone)
                 });
                 return response;
@@ -155,7 +155,7 @@ self.addEventListener('fetch', function (event) {
 });
 
 self.addEventListener('activate', function (event) {
-   var cacheWhitelist = ['v0.8'];
+   var cacheWhitelist = ['v0.9'];
 
    event.waitUntil(
        caches.keys().then(function (keyList) {
