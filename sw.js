8415a540295d2f097638aec24587c4c99f41f6de
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2019-07-19 06:28:47 +0000

    activation lien programme precamp

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open('v0.5').then(function (cache) {
+        caches.open('v0.5.1').then(function (cache) {
             return cache.addAll([
                 'index.html',
                 'comite.html',
@@ -130,7 +130,7 @@ self.addEventListener('install', function (event) {
                 'compteur/timeTo.css',
                 'compteur/jquery.time-to.min.js',
                 'precamp.html',
-                'precamp_samedi.html'
+                'precamp_samedi.html',
             ])
         })
     )
@@ -141,7 +141,7 @@ self.addEventListener('fetch', function (event) {
         caches.match(event.request).then(function (resp) {
             return resp || fetch(event.request).then(function (response) {
                 let responseClone = response.clone();
-                caches.open('v0.5').then(function (cache) {
+                caches.open('v0.5.1').then(function (cache) {
                     cache.put(event.request, responseClone)
                 });
                 return response;
@@ -153,7 +153,7 @@ self.addEventListener('fetch', function (event) {
 });
 
 self.addEventListener('activate', function (event) {
-   var cacheWhitelist = ['v0.5'];
+   var cacheWhitelist = ['v0.5.1'];
 
    event.waitUntil(
        caches.keys().then(function (keyList) {
